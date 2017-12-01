e746bed95278f29612d8b6e2e7bb61961ee0c131
Audrey Strelzoff <astrelzoff@ufl.edu>
2017-11-30 21:04:22 -0600

    Add custom javascript to change donate button on hover and click

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+// Custom Javascript
+// Set a global variable for the button and retrieve the figure with id of donate
+var theButton = document.getElementById('donate');
+
+// add an event listener for mouseover of the image and open a function
+theButton.addEventListener('mouseover', function () {
+    console.log('entering');
+    // Change the source of the image to the yellow button on mouseover
+    this.src = 'img/button_yellow.png';
+});
+
+// Add an event listener to the image for mouseout and open a function
+theButton.addEventListener('mouseout', function () {
+    console.log('leaving');
+    // Change the source of the image back to the red button on mouseout
+    this.src = 'img/button_red.png';
+});
+
+// Add an event listener to the image for click and open a function
+theButton.addEventListener('mousedown', function () {
+    console.log('clicky');
+    // Change the source of the image back to the blue button on mousedown
+    this.src = 'img/button_blue.png';
+});
+
+// jQuery Navigation Plug-in
 $(document).ready(function () {
     $('#menu-container').responsiveMenu();
 });
