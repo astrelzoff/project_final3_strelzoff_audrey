49055db7759dbc8422128d928b7ac4693a76f0a7
Audrey Strelzoff <astrelzoff@ufl.edu>
2017-11-30 21:05:43 -0600

    Comment out console log events

--- js/main.js
+++ js/main.js
@@ -4,21 +4,21 @@ var theButton = document.getElementById('donate');
 
 // add an event listener for mouseover of the image and open a function
 theButton.addEventListener('mouseover', function () {
-    console.log('entering');
+    // console.log('entering');
     // Change the source of the image to the yellow button on mouseover
     this.src = 'img/button_yellow.png';
 });
 
 // Add an event listener to the image for mouseout and open a function
 theButton.addEventListener('mouseout', function () {
-    console.log('leaving');
+    // console.log('leaving');
     // Change the source of the image back to the red button on mouseout
     this.src = 'img/button_red.png';
 });
 
 // Add an event listener to the image for click and open a function
 theButton.addEventListener('mousedown', function () {
-    console.log('clicky');
+    // console.log('clicky');
     // Change the source of the image back to the blue button on mousedown
     this.src = 'img/button_blue.png';
 });
